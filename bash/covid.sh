dbe4ddd4b0e80fd3b22b838d318c083c28074db2
vanschwm <vanschwm@mail.uc.edu>
2022-09-11 17:13:43 -0400

    Lab 2

--- bash/covid.sh
+++ bash/covid.sh
@@ -3,8 +3,8 @@
 DATA=$(curl https://api.covidtracking.com/v1/us/current.json)
 POSITIVE=$(echo $DATA | jq '.[0].positive')
 NEGATIVE=$(echo $DATA | jq '.[0].negative')
-DEATHS=$(echo $DATA | jq '.[0].deaths')
+DEATHS=$(echo $DATA | jq '.[0].death')
 HOSPITALIZED=$(echo $DATA | jq '.[0].hospitalized')
 TODAY=$(date)
 
-echo "On $TODAY, there were $POSITIVE positive covid cases, there were $NEGATIVE negative tests, and $HOSPITALIZED hospitalized"
+echo "On $TODAY, there were $POSITIVE positive covid cases, there were $NEGATIVE negative tests, there were $DEATHS deaths and $HOSPITALIZED hospitalized"
