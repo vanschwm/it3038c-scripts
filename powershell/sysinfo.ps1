16ff67f01d47acf88b0e0ed4ded0c3e1bad0080c
vanschwm <vanschwm@mail.uc.edu>
2022-09-15 22:30:00 -0400

    powershell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,5 @@
+﻿function getIP { 
+(Get-NetIPAddress).IPv4Address | select-string "192*" 
+}
+
+### I was removing all the commands I was typing and starting fresh on each one. I have a word document with everything I learned throughout the lab if you would like me to send it to you. 
