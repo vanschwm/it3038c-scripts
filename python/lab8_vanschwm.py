c90dfa18a37c957fc0df728318471f16ba5a4857
vanschwm <vanschwm@mail.uc.edu>
2022-11-17 16:42:11 -0500

    Organizing

--- python/lab8_vanschwm.py
+++
@@ -1,7 +0,0 @@
-import requests, re 
-from bs4 import BeautifulSoup
-
-r = requests.get("https://www.nike.com/w/mens-shoes-nik1zy7ok").content
-soup = BeautifulSoup(r,'html.parser')
-tags = soup.findAll("div",{"class":re.compile('(product-grid_items css-hvew4t)')})
-
